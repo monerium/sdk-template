0acd617fb277f43bc7110c49bd2898e82f5878e2
einaralex <einaralex@gmail.com>
2023-04-24 00:13:45 +0000

    chore: cleanup

--- src/pages/api/monerium.ts
+++ src/pages/api/monerium.ts
@@ -42,50 +42,5 @@ export default async function handler(
 
   cookies.set("refreshToken", refresh_token);
 
-  //   const ctx = await client.getAuthContext();
-  //   const user = await client.getProfile(profileId);
-
-  //   console.log('AuthContext', ctx);
-  //   console.log('User', user)
-
-  //   console.log("aha", ctx, profile, user);
-
   res.redirect("/");
-  //   res.status(200).end();
-
-  //   const queryParams = req.query;
-  //   if (queryParams?.code && queryParams?.state) {
-  //     let params = JSON.parse(
-  //       cookies?.get(queryParams.state as string) as string
-  //     );
-  //     const authorizationCode = queryParams?.code as string;
-
-  //     const headers = new Headers();
-  //     headers.append("content-type", "application/x-www-form-urlencoded"); // Required
-
-  //     if (authorizationCode && params && params?.client_id) {
-  //       await fetch("https://api.monerium.dev/auth/token", {
-  //         method: "POST",
-  //         body: new URLSearchParams({
-  //           client_id: params.client_id,
-  //           code: authorizationCode,
-  //           redirect_uri: params.redirect_uri,
-  //           grant_type: "authorization_code",
-  //           code_verifier: params.code_verifier,
-  //         }),
-  //         headers: headers,
-  //       }).then(async (data) => {
-  //         const response = await data.json();
-  //         const { access_token, profile, userId, refresh_token } = response;
-
-  //         // You should securely store the access_token and the refresh_token
-  //         // This is not secure, it exposes the access_token to the client:
-  //         cookies.set(profile, JSON.stringify(response));
-
-  //         res.redirect(`/user/${profile}`);
-  //       });
-  //     }
-  //   } else {
-  //     res.status(400).end();
-  //   }
 }
