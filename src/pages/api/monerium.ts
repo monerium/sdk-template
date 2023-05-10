7ac56f8da323c218a7dd1c39dffc86ac59072a06
Einar Alexander Eymundsson <einaralex@gmail.com>
2023-05-10 17:41:00 +0000

    Update monerium.ts

--- src/pages/api/monerium.ts
+++ src/pages/api/monerium.ts
@@ -12,27 +12,22 @@ export default async function handler(
 ) {
   const queryParams = req.query;
   const cookies = new Cookies(req, res);
-  const codeVerifier = cookies.get("codeVerifier");
-  console.log(
-    "||||",
-    "color:white; padding: 30px; background-color: darkgreen",
-    codeVerifier,
-    "||||"
-  );
 
-  const client = new MoneriumClient();
+  const codeVerifier = cookies.get("codeVerifier");
+
+  const emi = new MoneriumClient();
 
   console.log("middleware codeVerifier", codeVerifier);
   console.log("middleware queryParams", queryParams);
 
-  await client.auth({
+  await emi.auth({
     client_id: "654c9c30-44d3-11ed-adac-b2efc0e6677d",
     redirect_uri: "http://localhost:3000/api/monerium",
     code_verifier: codeVerifier as string,
     code: queryParams?.code as string,
   });
 
-  cookies.set("refreshToken", client.bearerProfile?.refresh_token);
+  cookies.set("refreshToken", emi.bearerProfile?.refresh_token);
 
   res.redirect("/");
 }
